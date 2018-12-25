6329ddf24ac019314145b7b367b3404c68e0736b
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-25 08:16:51 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,37 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    joins("INNER JOIN boats b ON b.captain_id = captains.id
+      INNER JOIN boat_classifications bc ON bc.boat_id = b.id
+      INNER JOIN classifications c ON bc.classification_id = c.id
+      WHERE c.name = 'Catamaran'")
+  end
+
+  def self.sailors
+    joins("INNER JOIN boats b ON b.captain_id = captains.id
+      INNER JOIN boat_classifications bc ON bc.boat_id = b.id
+      INNER JOIN classifications c ON bc.classification_id = c.id
+      WHERE c.name = 'Sailboat'").distinct
+  end
+
+  def self.talented_seafarers
+    ar = joins("INNER JOIN boats b ON b.captain_id = captains.id
+           INNER JOIN boat_classifications bc ON bc.boat_id = b.id
+           INNER JOIN classifications c ON bc.classification_id = c.id
+           WHERE c.name = 'Motorboat'")
+#           INTERSECT
+#           SELECT COUNT(*) FROM captains
+#           INNER JOIN boats b ON b.captain_id = captains.id
+#                   INNER JOIN boat_classifications bc ON bc.boat_id = b.id
+#                   INNER JOIN classifications c ON bc.classification_id = c.id
+#                   WHERE c.name = 'Sailboat'")
+  end
+
+  def self.non_sailors
+   ar = Boat.joins("INNER JOIN boat_classifications bc ON bc.boat_id = b.id
+              INNER JOIN classifications c ON bc.classification_id = c.id
+              WHERE c.name = 'Motorboat'").to_sql
+   binding.pry
+  end
 end
