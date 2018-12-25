0d74bb0ce9ba114a0efa0224853677a377cfbd73
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-25 11:12:07 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -24,8 +24,17 @@ class Captain < ActiveRecord::Base
   end
 
   def self.non_sailors
-   ar = Boat.joins("INNER JOIN boat_classifications bc ON bc.boat_id = b.id
-              INNER JOIN classifications c ON bc.classification_id = c.id
-              WHERE c.name = 'Motorboat'").to_sql
+    select(:id,:name).joins("
+    inner join Boats b on c.id = b.capitan_id
+    inner join BoatClassifications bc on b.id = bc.boat_id
+    inner join Classifications cl on bc.classification_id = cl.id
+    WHERE NOT EXISTS
+    (
+      select c1.name,b1.name,cl1.name from Capitans c1
+      inner join Boats b1 on c1.id = b1.capitan_id
+      inner join BoatClassifications bc1 on b1.id = bc1.boat_id
+      inner join Classifications cl1 on bc1.classification_id = cl1.id
+      WHERE c1.id = c.id AND cl1.name = 'Sailboat'
+      )").group("captains.id, captains.name")
   end
 end
